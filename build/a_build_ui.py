082bf3f9677ce06e6cb48c28ddf1698c99dce360
Crystal Baker <35317618+Crystal-Reshea@users.noreply.github.com>
2025-01-29 12:28:49 -0500

    11 new UI component illustrative example (#12)
    * Update components.json
    
    Added text and textarea for illustrative examples.
    
    * Update components.json
    
    Updated formatting
    
    * Updated custom step ui and variables
    
    Added illustrative example text area and variable. Updated name to reference in-context learning
    
    * Update LLM - Azure OpenAI In-context Learning.step
    
    * Update contributor and step name
    
    Updated contributor names and custom step names

--- build/a_build_ui.py
+++ build/a_build_ui.py
@@ -3,15 +3,15 @@ import json
 from py_sas_studio_custom_steps import CustomStep
 from datetime import datetime
 
-createdBy="Sundaresh Sankaran"
-modifiedBy="Sundaresh Sankaran"
+createdBy="Sundaresh Sankaran and Crystal Baker"
+modifiedBy="Crystal Baker"
+name= "LLM - Azure OpenAI In-context Learning"
 
-
-cs = CustomStep(type="code",createdBy="Sundaresh Sankaran", 
-              name="LLM - Azure OpenAI Zero-Shot Prompting",
-              displayName="LLM - Azure OpenAI Zero-Shot Prompting",
+cs = CustomStep(type="code",createdBy=createdBy, 
+              name=name,
+              displayName=name,
               creationTimeStamp=datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-              modifiedBy="Sundaresh Sankaran",
+              modifiedBy="Crystal Baker",
               modifiedTimeStamp=datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
 
 with open(os.path.join(os.getcwd(),"components.json"),"r") as f:
@@ -21,6 +21,7 @@ jsd = json.dumps(js)
 
 cs["ui"]=jsd
 
-cs.create_custom_step(custom_step_path=os.path.join(os.getcwd(),"..","LLM - Azure OpenAI Zero-Shot Prompting.step"))
+# cs.create_custom_step(custom_step_path=os.path.join(os.getcwd(),"..","LLM - Azure OpenAI Zero-Shot Prompting.step"))
+cs.create_custom_step(os.path.join(os.getcwd(),"..",name + ".step"))
 
 
